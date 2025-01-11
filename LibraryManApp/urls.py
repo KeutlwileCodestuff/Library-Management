7e6303ebccb01f3416cfa1a491a35c46681b3fd7
WeThinkCode Student <kmpolokeng023@student.wethinkcode.co.zagit config --global user.email kmpolokeng023@student.wethinkcode.co.za>
2025-01-11 10:34:20 +0200

    ui almost done

--- LibraryManApp/urls.py
+++ LibraryManApp/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('', Home),
     path('shop' ,Shopping ),
     path('save',Saving),
+    path('books' , BOOKS),
+    path('returns',RETURNS),
+    path('my-bag',MY_BAG),
+    path('readers',READERS)
 ]
--- LibraryManApp/views.py
+++ LibraryManApp/views.py
@@ -4,11 +4,23 @@ from django.contrib import admin
 
 
 def Home(request):
-    return render(request , 'index.html', context={} )
+    return render(request , 'index.html', context={"current_tab":"HOME" }  )
+
+def BOOKS(request):
+    return render(request , 'books.html', context={"current_tab":"BOOKS" }  )
+
+def READERS(request):
+    return render(request , 'readers.html', context={"current_tab":"READERS" }  )
+
+def RETURNS(request):
+    return render(request , 'returns.html', context={"current_tab":"RETURNS" }  )
+
+def MY_BAG(request):
+    return render(request , 'my_bag.html', context={"current_tab":"MY BAG" }  )
 
 def Shopping(request):
     return HttpResponse("this is the shopping page")
 
 def Saving(request):
     save_student = request.POST['Student name']
-    return HttpResponse("welcome " + save_student)
+    return render(request , 'welcome.html', context={'Student name': save_student} )
