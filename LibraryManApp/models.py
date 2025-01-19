d2c8e3b8baf02570bc07101093071d65bcad84ce
WeThinkCode Student <keutlwilempolokeng@gmail.com>
2025-01-19 23:32:37 +0200

    fixed error

--- LibraryManApp/models.py
+++ LibraryManApp/models.py
@@ -1,12 +1,12 @@
-# from django.db import models
+from django.db import models
 
-# class reader(models.Model):
-#     reader_name = models.CharField(max_length = 200)
-#     reader_contact = models.CharField(max_length = 200)
-#     reader_id = models.CharField(max_length = 200)
-#     reader_address = models.CharField(max_length = 200)
-#     reader_name = models.TextField()
-#     active = models.BooleanField(default=True)
+class reader(models.Model):
+    reader_name = models.CharField(max_length = 200)
+    reader_contact = models.CharField(max_length = 200)
+    reader_id = models.CharField(max_length = 200)
+    reader_address = models.CharField(max_length = 200)
+    reader_name = models.TextField()
+    active = models.BooleanField(default=True)
 
 
 
